30f7a923a8adcbc93aaaf7157ffb244cb62a449a
钟兆立 <525966315@qq.com>
2019-03-04 11:28:29 +0800

    授权取消

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,7 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
+    Userauthorization: false,
     loading: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
@@ -19,32 +20,31 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    loading.loading_open(this);
+    const that = this;
+    //延迟授权
+    setTimeout(function(){
+      if (app.globalData.userInfo) {
+        loading.loading_close(that);
+        that.setData({
+          userInfo: app.globalData.userInfo,
+          hasUserInfo: true,
+          
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
+      } else if (that.data.canIUse){
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        loading.loading_close(that);
+        app.userInfoReadyCallback = res => {
+          that.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
         }
-      })
-    }
+      } else {
+        loading.loading_close(that);
+      }
+    }, 500);
   },
   getUserInfo: function(e) {
     app.globalData.userInfo = e.detail.userInfo
